74c92baa05d138606db4447bcc175283188896e7
Sören Hentzschel <cadeyrn@Cadeyrns-Macbook-Pro.local>
2012-05-19 14:56:04 +0200

    more i18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,6 @@ module ApplicationHelper
     User.find_by_id(session[:id])
   end
   
-  # thanks to railscasts.com ;-)
   def link_to_add_fields(name, f, association)
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
