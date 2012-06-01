010af23a81a26251c61e15bc868354e0a59f9baf
Sören Hentzschel <kontakt@soeren-hentzschel.at>
2012-06-01 17:56:18 +0200

    repaired picture upload

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -17,7 +17,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/images"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -51,5 +51,10 @@ class PictureUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+  def filename
+    if original_filename
+      "#{Time.now.to_time.to_i}-#{original_filename}"
+    end
+  end
 
 end
