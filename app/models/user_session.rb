9cd27dd650798bd6d22511bf0e7f56e6fc1a12a6
Sören Hentzschel <kontakt@soeren-hentzschel.at>
2012-06-06 11:13:28 +0200

    changed ldap config

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,23 +1,23 @@
 class UserSession < ActiveRecord::Base
   attr_accessible :login, :password
-  
+
   # thanks to mediacube portfolio
   def initialize(options = {})
     super
     self.login = options[:login]
     self.password = options[:password]
   end
-  
+
   def save
     user = User.find_by_fhsid self.login
-    
+
     if !user || !ldap(self.login, self.password)
       return false
     else
       self.user_id = user.id
       return true
     end
-  end 
+  end
 
   private
 
@@ -27,8 +27,7 @@ class UserSession < ActiveRecord::Base
     if Rails.env.production?
       ldap = Net::LDAP.new(
         :host=>"denise.core.fh-salzburg.ac.at",
-        :port=>636,
-        :encryption => :simple_tls
+        :port=>389
       )
 
       return ldap.bind_as(:base => "uid=#{user},ou=People,ou=Urstein,o=fh-salzburg.ac.at,o=FHS", :password=>password) ? true : false
