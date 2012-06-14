5f32a1b0c3ede3fbdc9f645850154508f3448f7b
Sören Hentzschel <kontakt@soeren-hentzschel.at>
2012-06-14 11:02:50 +0200

    disabled https for testing

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,7 @@ module FhsMeetIdeas
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    
+
     config.assets.precompile += ['application.css', 'application.js']
   end
 end
