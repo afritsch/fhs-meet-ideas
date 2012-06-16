56d7f5a00149ffb278320418cb34b8faf1a210c7
Sören Hentzschel <kontakt@soeren-hentzschel.at>
2012-06-17 00:58:42 +0200

    small change

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -3,8 +3,7 @@ class VersionsController < ApplicationController
     @version = Version.find(params[:id])
     @version.reify ? @version.reify.save! : @version.item.destroy
 
-    link_name = params[:redo] == "true" ? I18n.t("paper_trail.undo") : I18n.t("paper_trail.redo")
-    link = view_context.link_to(link_name, revert_version_path(@version.next, :redo => !params[:redo]), :method => :post)
+    link = view_context.link_to(I18n.t("paper_trail.undo"), revert_version_path(@version.next, :redo => !params[:redo]), :method => :post)
 
     flash[:notice] = "#{I18n.t("paper_trail.undid")} #{I18n.t("paper_trail." + @version.event)}. #{link}"
     redirect_to @version.reify ? :back : projects_path
