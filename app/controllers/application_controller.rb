0b52632ebe2c137e15360f956e86a5309f9b61f3
Sören Hentzschel <kontakt@soeren-hentzschel.at>
2012-06-17 02:38:49 +0200

    small change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :set_locale unless Rails.env.test?
+  before_filter :set_locale
   before_filter :require_login
 
   protected
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::Base
   end
 
   def extract_locale_from_accept_language_header
-    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first unless request.env['HTTP_ACCEPT_LANGUAGE'] === nil
   end
 
   # authentication methods
