a25993086f5059a7bb1caf79a76be933d83686ee
Sören Hentzschel <kontakt@soeren-hentzschel.at>
2012-06-18 21:37:06 +0200

    send mail on new comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,12 @@ class CommentsController < ApplicationController
     @project = Project.find(params[:project_id])
     @comment = @project.comments.new(params[:comment])
     @comment.user_id = current_user.id
-    
+
     if @comment.save
+      @project.followups.each do |follower|
+        ProjectUpdateMailer.delay.project_update(User.find(follower.user_id), @project)
+      end
+
       flash[:notice] = t("projects.comment.saved")
       redirect_to project_path(@project) + "#" + dom_id(@comment)
     else
