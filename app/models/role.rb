5bac484da9a441296075abb82f59fad4793e82cd
Sören Hentzschel <kontakt@soeren-hentzschel.at>
2012-06-19 13:08:56 +0200

    fixed display of empty roles & vendor safe stylesheet (gradients)

--- app/models/role.rb
+++ app/models/role.rb
@@ -5,5 +5,5 @@ class Role < ActiveRecord::Base
   belongs_to :user
 
   validates_length_of :title, :within => 3..255, :too_short => I18n.t("validations.length.short", :attr => I18n.t("validations.attr.role_title"), :count => "%{count}"), :too_long => I18n.t("validations.length.long", :attr => I18n.t("validations.attr.role_title"), :count => "%{count}")
-  validates_length_of :name, :within => 3..255, :too_short => I18n.t("validations.length.short", :attr => I18n.t("validations.attr.role_name"), :count => "%{count}"), :too_long => I18n.t("validations.length.long", :attr => I18n.t("validations.attr.role_name"), :count => "%{count}"), :allow_nil => true
+  validates :name, :length => { :maximum => 255, :too_long => I18n.t("validations.length.long", :attr => I18n.t("validations.attr.role_name"), :count => "%{count}") }
 end
